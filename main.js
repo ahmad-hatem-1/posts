4b2af42df69c99d6275cfa4eaf5c7fbab7e92c72
‪ahmed hatem‬‏ <mr.ahmedhatem.mo@gmail.com>
2024-06-16 03:13:57 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 
+let users = document.getElementById("users_ul")
+let posts_ul = document.getElementById("posts_ul")
 function lis_click() {
     let lis = document.querySelectorAll('.users ul li');
     lis[0].classList.add("active")
@@ -13,53 +15,52 @@ function lis_click() {
     })
 
 }
-function getpost(id) {
-    let posts_ul = document.getElementById("posts_ul")
-    posts_ul .style.cssText = `filter: blur(10px);`
-    let request = new XMLHttpRequest()
-    request.open("GET", `https://jsonplaceholder.typicode.com/posts?userId=${id||1}`)
-    request.responseType = "json"
-    request.send()
-    request.onload = _ => {
-        if (request.status >= 200 && request.status < 300) {
+function posts(userId) {
+    posts_ul.style.cssText = `filter: blur(10px);`
+    fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId || 1}`)
+        .then(response => {
+            if (response.ok) {
+                return response.json()
+            }
+        }).then(data => {
+            posts_ul.style.cssText = `filter: blur(0px);`
+
             posts_ul.innerHTML = ""
-            for (items of request.response) {
+            for (items of data) {
                 posts_ul.innerHTML += `
-                 <li>
-            <h2>${items.title}</h2>
-            <hr>
-            <p>${items.body}</p>
+            <li>
+                <h2>${items.title}</h2>
+                <hr>
+                <p>${items.body}</p>
             </li>
             `
-        }
-        console.log(request.response[0])
-            posts_ul.style.cssText = `filter: blur(0px);`
-        }
-    }
-}
-getpost()
-function get_user (){  
-              let users = document.getElementById("users_ul")
-    let request = new XMLHttpRequest()
-    request.open("GET", "https://jsonplaceholder.typicode.com/users")
-    request.responseType = "json"
-    request.send()
-    request.onload = _ => {
-    if (request.status >= 200 && request.status < 300) {
-        
-            for (let i = 0 ; i < request.response.length ; i++) {
-                users.innerHTML += `
-                <li onclick="getpost(${request.response[i].id})">
-                <h5>${request.response[i].username}</h5>
-                <p>${request.response[i].email}</p>
-                 </li>
-                
-                `
-            
             }
-            lis_click() 
-    }
+        })
 }
-
-}
-get_user ()
+new Promise((resolve, reject) => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+        .then(response => {
+            if (response.ok) {
+                return response.json()
+            }
+            else (
+                reject("error")
+            )
+        })
+        .then(data => {
+            for (let i = 0; i < data.length; i++) {
+                users.innerHTML += `
+        <li onclick="posts(${data[i].id})">
+        <h5>${data[i].username}</h5>
+        <p>${data[i].email}</p>
+        </li>
+        `
+                lis_click()
+            }
+            resolve(userId = 1)
+        })
+}).then(_ => {
+    posts()
+}).catch(error => {
+    alert(error)
+})
